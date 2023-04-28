85559b2d2ee174d8c00a48222c6a847f7582df09
AyyanFawad <77777333+AyyanFawad@users.noreply.github.com>
2023-04-28 23:44:17 +0500

    Add files via upload

---
+++ flutter-todo-app-master/lib/screens/LoginScreen.dart
@@ -0,0 +1,306 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../constants/colors.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+final String authEndpoint = 'https://example.com/api/auth';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool? isRememberMe = false;
+
+  Future<void> check_credentials(String email, String password) async {
+    final http.Response response = await http.post(
+      Uri.parse(authEndpoint),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseBody = jsonDecode(response.body);
+      final String authToken = responseBody['token'];
+      // TODO: Store authToken locally on the device for subsequent requests
+
+      Navigator.pushNamed(context, '/main');
+    } else {
+      final Map<String, dynamic> responseBody = jsonDecode(response.body);
+      final String errorMessage = responseBody['message'];
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text(errorMessage),
+            actions: [
+              TextButton(
+                child: Text('OK'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  Widget buildEmail() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Email',
+          style: TextStyle(
+              color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 10),
+        Container(
+            alignment: Alignment.centerLeft,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 6,
+                      offset: Offset(0, 2))
+                ]),
+            height: 60,
+            child: TextField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              style: TextStyle(color: Colors.black87),
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(top: 14),
+                  prefixIcon: Icon(Icons.email, color: tdBlack),
+                  hintText: 'Email',
+                  hintStyle: TextStyle(color: Colors.black38)),
+            ))
+      ],
+    );
+  }
+
+  Widget buildPassword() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Password',
+          style: TextStyle(
+              color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 10),
+        Container(
+            alignment: Alignment.centerLeft,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 6,
+                      offset: Offset(0, 2))
+                ]),
+            height: 60,
+            child: TextField(
+              controller: passwordController,
+              obscureText: true,
+              style: TextStyle(color: Colors.black87),
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(top: 14),
+                  prefixIcon: Icon(Icons.lock, color: tdBlack),
+                  hintText: 'Password',
+                  hintStyle: TextStyle(color: Colors.black38)),
+            ))
+      ],
+    );
+  }
+
+  Widget buildForgotPassBtn() {
+    return Container(
+        alignment: Alignment.centerRight,
+        child: TextButton(
+          onPressed: () => print("Forgot Pass Pressed"),
+          child: Text(
+            'Forgot Password?',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ));
+  }
+
+  Widget buildRememberCB() {
+    return Container(
+      height: 20,
+      child: Row(
+        children: <Widget>[
+          Theme(
+              data: ThemeData(unselectedWidgetColor: Colors.white),
+              child: Checkbox(
+                value: isRememberMe,
+                checkColor: tdBlue,
+                activeColor: Colors.white,
+                onChanged: (value) {
+                  setState(() {
+                    isRememberMe = value;
+                  });
+                },
+              )),
+          Text(
+            'Remeber Me',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildLoginBtn() {
+    return Container(
+        padding: EdgeInsets.symmetric(vertical: 25),
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: () async {
+            // Send the email and password to the backend for authentication
+            final http.Response response = await http.post(
+              Uri.parse(authEndpoint),
+              headers: {'Content-Type': 'application/json'},
+              body: jsonEncode({
+                'email': emailController.text,
+                'password': passwordController.text,
+              }),
+            );
+
+            // If the response indicates successful authentication,
+            // store the authentication token and navigate to the main screen
+            if (response.statusCode == 200) {
+              final Map<String, dynamic> responseBody =
+                  jsonDecode(response.body);
+              final String authToken = responseBody['token'];
+              // TODO: Store authToken locally on the device for subsequent requests
+
+              // Navigate to the main screen
+              Navigator.pushNamed(context, '/main');
+            } else {
+              // If the authentication failed, display an error message
+              final Map<String, dynamic> responseBody =
+                  jsonDecode(response.body);
+              final String errorMessage = responseBody['message'];
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('Error'),
+                    content: Text(errorMessage),
+                    actions: [
+                      TextButton(
+                        child: Text('OK'),
+                        onPressed: () => Navigator.of(context).pop(),
+                      ),
+                    ],
+                  );
+                },
+              );
+            }
+          },
+          style: ElevatedButton.styleFrom(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            padding: EdgeInsets.all(15),
+            backgroundColor: Colors.white,
+          ),
+          child: Text(
+            'Login',
+            style: TextStyle(
+                color: tdBlue, fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ));
+  }
+
+  Widget buildSignupBtn() {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, '/RegisterScreen');
+      },
+      child: RichText(
+        text: TextSpan(children: [
+          TextSpan(
+              text: 'Don\'t have an account?',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500)),
+          TextSpan(
+              text: "Sign Up",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold))
+        ]),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: AnnotatedRegion<SystemUiOverlayStyle>(
+            value: SystemUiOverlayStyle.light,
+            child: GestureDetector(
+              child: Stack(children: <Widget>[
+                Container(
+                    height: double.infinity,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                          Color(0xFF16A0E5),
+                          Color(0xFF16A3E5),
+                          Color(0xFF16A5E5),
+                          Color(0xFF16A9E5)
+                        ])),
+                    child: SingleChildScrollView(
+                      physics: AlwaysScrollableScrollPhysics(),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 25,
+                        vertical: 120,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'Sign In',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 50),
+                          buildEmail(),
+                          SizedBox(height: 20),
+                          buildPassword(),
+                          buildForgotPassBtn(),
+                          buildRememberCB(),
+                          buildLoginBtn(),
+                          buildSignupBtn(),
+                        ],
+                      ),
+                    ))
+              ]),
+            )));
+  }
+}
