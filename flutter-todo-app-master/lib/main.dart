6a3bf9391a95f7cb7f7cfb523202e8d1dd345f9f
AyyanFawad <77777333+AyyanFawad@users.noreply.github.com>
2023-05-02 15:05:43 +0500

    Add files via upload

--- flutter-todo-app-master/lib/main.dart
+++ flutter-todo-app-master/lib/main.dart
@@ -1,19 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import './screens/home.dart';
+import './screens/Home.dart';
 import './screens/LoginScreen.dart';
 import './screens/trash.dart';
-import 'screens/HeroPage.dart';
+import './screens/HeroPage.dart';
+import './screens/RegisterScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'screens/RegisterScreen.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // DocumentSnapshot snapshot = await FirebaseFirestore.instance
+  //     .collection("users")
+  //     .doc("cc29z7oTyp0PDv9wUFXA")
+  //     .get();
+
+  // print(snapshot.data().toString());
+  // Map<String, dynamic> newUserData = {
+  //   "name": "Zain Hassan",
+  //   "email": "zain@gmail.com"
+  // };
+  // await _firestore.collection("users").add(newUserData);
+  // print("newusersaved");
+
   runApp(const MyApp());
 }
 
@@ -30,13 +46,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HeroPage(),
+      home: (FirebaseAuth.instance.currentUser != null) ? Home() : HeroPage(),
       routes: {
         '/login': (context) => LoginScreen(),
         '/home': (context) => Home(),
         '/register': (context) => RegisterScreen(),
+        '/hero': (context) => HeroPage(),
       },
     );
   }
 }
-
